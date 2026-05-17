766a563526319127310b37652b50a0e581f7616a
otis m <murphyot@sheridancollege.ca>
2026-05-17 15:48:48 -0400

    Started on forward movement and roation code

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -5,12 +6,39 @@ public class Movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // asd
+       
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        forwardMovement(1);
+        //RotateTowardsMouse();
     }
+
+    void forwardMovement(float speed)
+    {
+        Vector3 pos =  transform.localPosition;
+        pos += Vector3.up * speed * Time.deltaTime;
+
+        transform.position = pos;
+
+
+    }
+
+
+    void RotateTowardsMouse()
+    {
+        Quaternion rot = transform.rotation;
+        rot.z += 15 *  Time.deltaTime;
+        transform.rotation = rot;
+
+    }
+
+
+
 }
