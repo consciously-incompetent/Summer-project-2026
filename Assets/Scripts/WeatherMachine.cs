34dd93b058fe3b2d4bab9a4f11b57a64172f2d72
otis m <murphyot@sheridancollege.ca>
2026-05-27 11:33:30 -0400

    started on weatrherMchine

---
+++ Assets/Scripts/WeatherMachine.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class WeatherMachine : MonoBehaviour
+{
+    public float timeBetweenSpawn;
+
+    public GameObject Wind;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void SpawnObject(GameObject creation)
+    {
+
+    }
+
+
+}
