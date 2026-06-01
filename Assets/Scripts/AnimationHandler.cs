75f170f0edc188600e49bcb44de257693c29379e
otis m <murphyot@sheridancollege.ca>
2026-06-01 16:49:59 -0400

    did some animtation stuff

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,20 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    public AnimatorController controller;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+}
